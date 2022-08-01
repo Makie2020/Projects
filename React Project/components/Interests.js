f9e40d6debef17fa19631432aca339b1f0b6eeb7
Makie2020 <69918471+Makie2020@users.noreply.github.com>
2022-08-01 17:59:45 +0200

    Add files via upload

---
+++ React Project/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return(
+        <div id="interests">
+            <h3>Interests</h3>
+            <p id="ptekst">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}
---
+++ React Project/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Info from "./components/Info"
+import About from "./components/About"
+import Interests from "./components/Interests"
+import Footer from "./components/Footer"
+
+ReactDOM.render(
+    <div id="mainContent">
+        <Info />
+        <About />
+        <Interests />
+        <Footer />
+    </div>,
+    document.getElementById("root")
+)
