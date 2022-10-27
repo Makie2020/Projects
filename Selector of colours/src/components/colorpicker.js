4a4008197dc05200267496e6e363f8d662aea569
Marieke <69918471+Makie2020@users.noreply.github.com>
2022-10-27 16:46:47 +0200

    Add files via upload

---
+++ Selector of colours/src/components/colorpicker.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { CompactPicker } from 'react-color';
+import './colorpicker.css';
+
+class Colorpicker extends React.Component {
+
+    render() {return <CompactPicker />
+    }
+  }
+
+export default Colorpicker;
